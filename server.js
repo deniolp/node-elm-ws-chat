74f47c769ce066119df8184683c17bed210756fc
deniolp <deniolp@gmail.com>
2021-07-24 20:50:51 +0300

    Implementing sending messages to all clients

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const {Server} = require(`ws`);
 const {v4} = require(`uuid`);
 
 const clients = {};
+const messages = [];
 
 const webSocketSever = new Server({port: 8000});
 
@@ -14,7 +15,11 @@ webSocketSever.on(`connection`, (ws) => {
     console.log(`New client was connected: ${id}`);
 
     ws.on(`message`, (rawMessage) => {
-        //
+        const {name, message} = JSON.parse(rawMessage);
+        messages.push({name, message});
+        for (const id in clients) {
+          clients[id].send(JSON.stringify([{name, message}]));
+        }
     })
 
     ws.on(`close`, () => {
